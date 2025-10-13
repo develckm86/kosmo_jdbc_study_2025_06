21b94243c5849b3e38ada06993bcf1e94a97980a
kyoungmin <develckm86@gmail.com>
2025-10-13 13:11:08 +0900

    문제풀이

---
+++ src/main/java/org/example/hw/H02PSTMT.java
@@ -0,0 +1,76 @@
+package org.example.hw;
+
+import java.sql.*;
+
+public class H02PSTMT {
+    public static void main(String[] args) {
+        String url="jdbc:oracle:thin:@//localhost:1521/XEPDB1";
+        String user="scott";
+        String pw="tiger";
+        try (Connection conn = DriverManager.getConnection(url,user,pw)){
+            try (PreparedStatement pstmt=conn.prepareStatement("DELETE FROM EMP WHERE empno IN (?,?)")){
+                pstmt.setInt(1,9001);
+                pstmt.setInt(2,9002);
+                int del=pstmt.executeUpdate();
+                if(del==1){
+                    System.out.println("9001 삭제 성공");
+                }
+            }
+
+            try(Statement stmt=conn.createStatement()){
+               String sql="INSERT INTO EMP (empno,ename,job,sal,deptno) " +
+                       "VALUES (9001,'ALLEN2','SALESMAN',1600.00,30)";
+               int insert=stmt.executeUpdate(sql);
+               if(insert==1){
+                   System.out.println("ALLEN2 등록성공");
+               }
+               //statement 는 일회용
+            }
+            String sql="INSERT INTO EMP (empno,ename,job,sal,deptno) VALUES (?,?,?,?,?)";
+
+            try (PreparedStatement psmt=conn.prepareStatement(sql)){
+                psmt.setInt(1,9002);
+                psmt.setString(2,"SMITH2");
+                psmt.setString(3,"CLERK");
+                psmt.setInt(4,800);
+                psmt.setInt(5,20);
+                int insert=psmt.executeUpdate();
+                if(insert>0){
+                    System.out.println("SMITH2 등록성공");
+                }
+
+            }
+
+
+            try(Statement stmt=conn.createStatement()){
+                ResultSet rs=stmt.executeQuery("SELECT * FROM EMP");
+                //String str="";
+                StringBuilder sb=new StringBuilder();
+                while (rs.next()){
+                    int empno=rs.getInt("empno");
+                    String ename=rs.getString("ename");
+                    String job=rs.getString("job");
+                    double sal=rs.getDouble("sal");
+                    int deptno=rs.getInt("deptno");
+                    //str+=empno+" | "+ename+" | "+ job+" | "+sal+" | "+deptno;
+                    //문자열 더하기는 객체를 많이 생성 => 메모리 많이 사용
+                    sb.append(empno);
+                    sb.append(" | ");
+                    sb.append(ename);
+                    sb.append(" | ");
+                    sb.append(job);
+                    sb.append(" | ");
+                    sb.append(sal);
+                    sb.append(" | ");
+                    sb.append(deptno);
+                    sb.append(" \n ");
+                }
+                //System.out.println(str);
+                System.out.println(sb.toString());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
