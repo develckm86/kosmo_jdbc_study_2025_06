f09e7f8b04eb2abceaf8d4bea3a1f48355813508
kyoungmin <develckm86@gmail.com>
2025-10-17 10:34:40 +0900

    emp dto dao null 및 localdate 처리

--- src/main/java/org/example/L05EmpDaoImp.java
+++ src/main/java/org/example/L05EmpDaoImp.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,10 +41,37 @@ public class L05EmpDaoImp implements L05EmpDao{
         rs=pstmt.executeQuery();
         empList=new ArrayList<>();
         while (rs.next()){
+            //comm,sal,hiredate,job,mgr
+            int empno=rs.getInt("empno");
+            String ename=rs.getString("ename");
+            //자바는 기본형이 null을 참조하는 것이 불가능!=> 기본형의 없음===0
+            //기본형타입의 자료형 랩퍼클래스(Byte,Short,Integer,Long, Float,Double)
+            //int deptno=rs.getInt("deptno"); //null:부서가없은, 0: 부서번호가 0
+            //int mgr=rs.getInt("mgr"); //null:상사가없음 ,0: 상사번호가 0
+            //new Integer(10)
+            BigDecimal deptnoDec=rs.getBigDecimal("deptno");
+            Integer deptno= (deptnoDec!=null)? deptnoDec.intValue() : null;
+            BigDecimal mgrDec=rs.getBigDecimal("mgr");
+            Integer mgr=(mgrDec!=null)?mgrDec.intValue():null;
+            BigDecimal commDec=rs.getBigDecimal("comm");
+            Double comm=(commDec!=null)?commDec.doubleValue():null;
+            BigDecimal salDec=rs.getBigDecimal("sal");
+            Double sal=(salDec!=null)?salDec.doubleValue():null;
+            //null일수 있는 기본형 데이터 필드는 Object로 가져와서 랩퍼클래스로 변환
+            //ResultSet DB에서 가져온 Date를 java.sql.Date or java.util.Date 로 반환
+            //요즘 자바에서는 LocalDate* 를 많이 사용
+            java.sql.Date hiredate=rs.getDate("hiredate");
+            LocalDate hiredateLocal=(hiredate!=null)?hiredate.toLocalDate():null;
+            //LocalDate.parse("2025-09-25")
             L05EmpDto emp=new L05EmpDto();
-            emp.setEmpno(rs.getInt("empno"));
-            emp.setEname(rs.getString("ename"));
-            emp.setDeptno(rs.getInt("deptno"));
+            emp.setEmpno(empno);
+            emp.setEname(ename);
+            emp.setComm(comm);
+            emp.setSal(sal);
+            emp.setMgr(mgr);
+            emp.setJob(rs.getString("job"));
+            emp.setDeptno(deptno);
+            emp.setHiredate(hiredateLocal);
             empList.add(emp);
         }
         return empList;
--- src/main/java/org/example/L05EmpDto.java
+++ src/main/java/org/example/L05EmpDto.java
@@ -14,12 +14,76 @@ DEPTNO*/
     private int empno;
     private String ename;
     private String job;
-    private int mgr;
+    private Integer mgr;
 //   private  java.util.Date hiredate;
     private LocalDate hiredate;
-    private double sal;
-    private double comm;
-    private int deptno;
+    private Double sal;
+    private Double comm;
+    private Integer deptno;
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public LocalDate getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(LocalDate hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+
+    public Double getComm() {
+        return comm;
+    }
+
+    public void setComm(Double comm) {
+        this.comm = comm;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
 
     @Override
     public String toString() {
@@ -34,56 +98,7 @@ DEPTNO*/
                 ", deptno=" + deptno +
                 "}\n";
     }
-
     //getter setter 캡슐화
-    public void setEmpno(int empno){
-        this.empno=empno;
-    }
-    public int getEmpno(){
-        return this.empno;
-    }
-    public void setEname(String ename){
-        this.ename=ename;
-    }
-    public String getEname(){
-        return this.ename;
-    }
     //거의 대부분의 개발툴은 get set 자동완성 재공
     //lombok : 컴파일시 자동완성하는 라이브러리 => spring
-    public String getJob() {
-        return job;
-    }
-    public void setJob(String job) {
-        this.job = job;
-    }
-    public LocalDate getHiredate() {
-        return hiredate;
-    }
-    public void setHiredate(LocalDate hiredate) {
-        this.hiredate = hiredate;
-    }
-    public double getComm() {
-        return comm;
-    }
-    public void setComm(double comm) {
-        this.comm = comm;
-    }
-    public int getDeptno() {
-        return deptno;
-    }
-    public void setDeptno(int deptno) {
-        this.deptno = deptno;
-    }
-    public double getSal() {
-        return sal;
-    }
-    public void setSal(double sal) {
-        this.sal = sal;
-    }
-    public int getMgr() {
-        return mgr;
-    }
-    public void setMgr(int mgr) {
-        this.mgr = mgr;
-    }
 }
