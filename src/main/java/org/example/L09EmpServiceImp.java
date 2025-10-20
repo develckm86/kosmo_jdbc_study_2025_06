6a101ca00a28247e2a334c36605df6462dcaa0f8
kyoungmin <develckm86@gmail.com>
2025-10-20 15:37:53 +0900

    emp service 수정 구현 및 테스트

--- src/main/java/org/example/L09EmpServiceImp.java
+++ src/main/java/org/example/L09EmpServiceImp.java
@@ -16,42 +16,81 @@ public class L09EmpServiceImp implements L09EmpService{
         this.empDao=new L05EmpDaoImp(conn);
         this.deptDao=new L07DeptDaoImp(conn);
     }
+    //트랜잭션: 데이터 수정이 2번이상 일어날때 문제가 생기면 세이브포인트로 되돌림
+    //register : 2번이상 수정(DML insert,update,delete)하지 않기 때문에 트랜잭션관리 필요 없음
+    //수업이니까 작성함!!!
 
     @Override
     public boolean register(L05EmpDto emp) throws SQLException, IllegalArgumentException {
-        //사번,이름이 있는지
-        //이름은 길이가 10이하인지 검사
-        //급여와 커미션이 있으면 0보다큰지
-        //사번이 이미 등록되어 있는지 (대표키)
-        //부서번호가 있다면 부서번호가 있는지(참조의 무결성)
-        //상사번호(mgr)가 없으면 오류!(참조의 무결성!!)
-        //등록
-        //EmpDto.empno int 없으면 0
-        if(emp.getEmpno()<=0) throw new IllegalArgumentException("사번을 입력하세요!");
-        //String.isBlank() : "", "   "
-        if(emp.getEname()==null || emp.getEname().isBlank()) throw new IllegalArgumentException("이름을 입력하세요");
+        try{
+            //jdbc conn 의 설정  autoCommit true
+            conn.setAutoCommit(false);
+            conn.commit(); // save point
+            //사번,이름이 있는지
+            //이름은 길이가 10이하인지 검사
+            //급여와 커미션이 있으면 0보다큰지
+            //사번이 이미 등록되어 있는지 (대표키)
+            //부서번호가 있다면 부서번호가 있는지(참조의 무결성)
+            //상사번호(mgr)가 없으면 오류!(참조의 무결성!!)
+            //등록
+            //EmpDto.empno int 없으면 0
+            if(emp.getEmpno()<=0) throw new IllegalArgumentException("사번을 입력하세요!");
+            //String.isBlank() : "", "   "
+            if(emp.getEname()==null || emp.getEname().isBlank()) throw new IllegalArgumentException("이름을 입력하세요");
 
-        if(emp.getSal()!=null && emp.getSal()<0) throw new IllegalArgumentException("급여는 0보다 커야합니다.");
-        if(emp.getComm()!=null && emp.getComm()<0) throw new IllegalArgumentException("커미션은 0보다 커야합니다.");
+            if(emp.getSal()!=null && emp.getSal()<0) throw new IllegalArgumentException("급여는 0보다 커야합니다.");
+            if(emp.getComm()!=null && emp.getComm()<0) throw new IllegalArgumentException("커미션은 0보다 커야합니다.");
 
-        L05EmpDto existEmp=empDao.findByEmpno(emp.getEmpno()); //이미 등록된 사번이 있나?
-        if(existEmp!=null) throw new IllegalArgumentException("이미 등록된 사번입니다.");
+            L05EmpDto existEmp=empDao.findByEmpno(emp.getEmpno()); //이미 등록된 사번이 있나?
+            if(existEmp!=null) throw new IllegalArgumentException("이미 등록된 사번입니다.");
 
-        L07DeptDto existDept=deptDao.findByDeptno(emp.getDeptno());//등록하려는 부서가 존재하나
-        List<L07DeptDto> depts=deptDao.findAll();
-        List<Integer> deptnos=new ArrayList<>();
-        for (L07DeptDto dept :depts){
-            deptnos.add(dept.getDeptno());
+            //Emp.deptno null 허용
+            if(emp.getDeptno()!=null){
+                L07DeptDto existDept=deptDao.findByDeptno(emp.getDeptno());//등록하려는 부서가 존재하나
+                List<L07DeptDto> depts=deptDao.findAll();
+                List<Integer> deptnos=new ArrayList<>();
+                for (L07DeptDto dept :depts){
+                    deptnos.add(dept.getDeptno());
+                }
+                if(existDept==null) throw new IllegalArgumentException("존재하는 부서번호를 입력하세요!"+deptnos);
+            }
+            //Emp.mrg (상사의 사번) null 허용
+            if(emp.getMgr()!=null){
+                L05EmpDto existMrg=empDao.findByEmpno(emp.getMgr());
+                if(existMrg==null) throw new IllegalArgumentException("상사가 존재하지 않습니다.mgr을 확인");
+            }
+            int insert=empDao.insertOne(emp);
+            return insert==0;
+        } catch (Exception e) {
+            conn.rollback();
+            throw e; //오류가 뜨면 오류를 바로 위임
         }
-        if(existDept==null) throw new IllegalArgumentException("존재하는 부서번호를 입력하세요!"+deptnos);
-
-        int insert=empDao.insertOne(emp);
-        return insert==0;
     }
 
     @Override
     public boolean modify(L05EmpDto emp) throws SQLException, IllegalArgumentException {
-        return false;
+        //트랜잭션 연습!!!
+        //사번 이름 꼭 존재
+        //급여와 커미션 > 0
+        //수정하려는 사원이 없다=> 존재하지 않느 사원은 수정불가
+        //수정하려는 부서번호가 없다=> 존재하지 않는 부서번호!
+        if(emp.getEmpno()==0)throw new IllegalArgumentException("수정할 사번을 입력하세요!");
+        if(emp.getEname()==null || emp.getEname().isBlank()) throw new IllegalArgumentException("이름은 꼭 입력하세요!");
+        if(emp.getSal()!=null && emp.getSal()<0) throw new IllegalArgumentException("급여는 0보다 큽니다.");
+        if(emp.getComm()!=null && emp.getComm()<0) throw new IllegalArgumentException("커미션는 0보다 큽니다.");
+        //유효성(Valid, Validation)검사
+        L05EmpDto existEmp=empDao.findByEmpno(emp.getEmpno());
+        if(existEmp==null)throw new IllegalArgumentException("수정할 사원이 없습니다.(이미 삭제된 사원입니다. 등록하고 수정하세요.)");
+        if(emp.getDeptno()!=null){
+            L07DeptDto existDept=deptDao.findByDeptno(emp.getDeptno());
+            if(existDept==null) throw  new IllegalArgumentException("존재하지 않는 부서번호입니다. 확인하세요!");
+        }
+        if(emp.getMgr()!=null){
+            L05EmpDto existMgr=empDao.findByEmpno(emp.getMgr());
+            if(existMgr==null) throw new IllegalArgumentException("존재하지 않는 상사번호입니다. 확인하세요!");
+        }
+
+        return empDao.updateOne(emp)==1;
     }
     //service : dao=1:1 (서비스가 단순한 경우)
     @Override
--- src/main/java/org/example/L10EmpServiceTest.java
+++ src/main/java/org/example/L10EmpServiceTest.java
@@ -12,16 +12,21 @@ public class L10EmpServiceTest {
             System.out.println(empService.readAll());
             System.out.println("readlByEname 테스트");
             System.out.println(empService.readByEname("scott"));
-
+            System.out.println("6666삭제 :"+empService.remove(6666));
             L05EmpDto emp=new L05EmpDto();
             emp.setEmpno(6666);
             emp.setEname("테스트");
             emp.setSal(500.0);
             emp.setComm(99.0);
-            emp.setMgr(1234);
+            emp.setMgr(7788);
             emp.setDeptno(40); //참조의무결성 위반!!!
             System.out.println("등록 성공:"+empService.register(emp));
             System.out.println("readOne 테스트 6666");
+            System.out.println(empService.readOne(6666));
+
+            emp.setSal(1000.0);
+            emp.setMgr(1111);
+            System.out.println("수정 성공:"+empService.modify(emp));
             System.out.println(empService.readOne(6666));
 
         } catch (SQLException e) {
